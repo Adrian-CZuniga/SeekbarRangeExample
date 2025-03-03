c200432ff136c1386aa7b202c1a2e0faa0ce5038
Adrian <aadriancz45@gmail.com>
2025-03-03 00:37:12 -0600

    Initial commit

---
+++ seekbarrangecustom/src/main/java/com/example/seekbarrangecustom/SeekbarRangeCustom.kt
@@ -0,0 +1,10 @@
+package com.example.seekbarrangecustom
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class SeekbarRangeCustom
+@JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0)
+    : ConstraintLayout(context, attrs, defStyleAttr) {
+}
